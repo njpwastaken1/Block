35a2f6da56e4613a25ddb20616edf367a2931e4b
nathan <97966664+njpwastaken1@users.noreply.github.com>
2023-09-10 21:33:18 +0100

    Add files via upload

---
+++ block/images/script.js
@@ -0,0 +1,13 @@
+$(function () {
+    $('.multiple-items').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 3,
+      arrows: false,
+      autoplay: true,
+      autoplaySpeed: 6000,
+      dots: false,
+      centerMode: true,
+      centerPadding: '0',
+    });
+  });
